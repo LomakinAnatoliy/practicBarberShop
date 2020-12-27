324f86cc5d03734f350dd4f4a1a22ef5720cc34f
LomakinAnatoliy <lomakin90anatoliy@gmail.com>
2020-12-27 18:41:45 +0200

    laid out an adaptive block - header

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 // all other code
-  console.log('dfdsfsdfsdfsdf');
 // Lazy loading img & background images using intersection observer
 // Reference: https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/
 // Using example: <img class="lazy" src="thumb.gif" data-src="real-img.jpg" data-srcset="real-img@1x.jpg 1x, real-img@2x.jpg 2x">
